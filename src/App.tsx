8424af23b5d49f5a78e865455e881b8f78859abe
Bryce Mercines <nplixel@gmail.com>
2020-08-06 02:28:41 +0800

    completed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact>
+              <h1>Home</h1>
+            </Route>
+
+            <Route path="/map">
+              <h1>Map</h1>
+            </Route>
+
+            <Route path="/search">
+              <h1>Search</h1>
+            </Route>
+
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
