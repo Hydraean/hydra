72208bb57b14de7f6be2c19e4f751b0a02d35ea1
Bryce Mercines <nplixel@gmail.com>
2020-08-07 01:55:53 +0800

    minor UI adjustments

---
+++ src/components/GridLogo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const GridLogo = () => {
+  return (
+    <div id="grid-logo">
+      <svg width="989px" height="895px" viewBox="0 0 989 895" version="1.1">
+        <g id="Aug-3-Sprint" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" opacity="0.300000012">
+          <g id="Main-Landing-page" transform="translate(-418.000000, 0.000000)" stroke="#E1E1E1" stroke-width="0.5">
+            <path d="M545.684372,-8 L729.031649,330.244965 L759.017787,274.783746 L605.697007,-8 L731.251349,-8 L821.250123,161.122483 L914.598626,-8 L1005.72743,161.122483 L1097.9459,-8 L1223.50025,-8 L792.878312,780.258183 L1161.18719,780.258183 L1223.50025,894 L605.737365,894 L943.252944,274.582362 L913.83182,217.953115 L546.209028,894 L419,894 L667.122179,442.979862 L419,-8 L545.684372,-8 Z M1406,-8 L1162.11543,442.979862 L1406,894 L1284.07789,894 L1097.98626,554.02313 L1012.10402,715.412458 L886.065383,715.412458 L1284.80434,-8 L1406,-8 Z" id="Combined-Shape"></path>
+          </g>
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+export default GridLogo;
