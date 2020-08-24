cc95b4f80622072ff049f52e24f9be7fa3fecba8
Bryce Mercines <nplixel@gmail.com>
2020-08-24 11:48:20 +0800

    render update for online device report interface

--- src/components/DeviceDemo.tsx
+++ src/components/DeviceDemo.tsx
@@ -21,7 +21,9 @@ const DeviceDemo = (props: any) => {
                 <div className="col-xl-5 col-lg-6 col-md-8 px-5">
                   <img src="/hlogo.png" alt="h-logo" className="landing-logo" />
                   <h1 className="text-white">Seantinel</h1>
-                  <p className="text-lead text-active">Report Demo</p>
+                  <p className="text-lead text-active">
+                    Reporting Interface Demo
+                  </p>
                 </div>
               </div>
             </div>
@@ -44,11 +46,92 @@ const DeviceDemo = (props: any) => {
         </div>
       </div>
 
-      <div className="container">
-        <div className="mb-7">
-          <div className="row justify-content-center">
-            <div className="card">
-              <button className="btn btn-primary btn-lg">Send Report</button>
+      <div className="container mt--8 pb-5">
+        {/* Table */}
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-md-8">
+            <div className="card bg-secondary border-0">
+              <div className="card-header bg-transparent pb-5">
+                <div className="text-muted text-center mt-2 mb-4">
+                  <small>
+                    Set your location, and complete the form before submitting
+                  </small>
+                </div>
+                <div className="text-center">
+                  <button className="btn btn-dark btn-icon">
+                    <i className="la la-map-marker mr-2" />
+                    Set Location
+                  </button>
+                </div>
+              </div>
+              <div className="card-body px-lg-5 py-lg-5">
+                <div className="text-center mb-4">
+                  <strong>Report Details:</strong>
+                </div>
+                <form role="form">
+                  <div className="form-group">
+                    <div className="input-group input-group-merge input-group-alternative mb-3">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">
+                          <i className="la la-user" />
+                        </span>
+                      </div>
+                      <input
+                        className="form-control"
+                        placeholder="Name"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="input-group input-group-merge input-group-alternative mb-3">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">
+                          <i className="la la-map" />
+                        </span>
+                      </div>
+                      <input
+                        className="form-control"
+                        placeholder="FMA Area / Location"
+                        type="text"
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="input-group input-group-merge input-group-alternative">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">
+                          <i className="la la-list" />
+                        </span>
+                      </div>
+                      <textarea
+                        className="form-control"
+                        placeholder="Report Details"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="row my-4">
+                    <div className="col-12">
+                      <div className="text-center">
+                        <label>
+                          <small className="text-muted">
+                            <i className="la la-exclamation-circle text-danger" />{" "}
+                            For Demonstration purposes only. All provided
+                            information is this form will be deleted after a
+                            period of time.
+                          </small>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <button type="button" className="btn btn-primary mt-4">
+                      <i className="la la-send mr-2" /> Submit Report
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
